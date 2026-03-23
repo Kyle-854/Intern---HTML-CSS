cc8cd3eefa87993c34526ec471d6192852804450
Kyle <anhkietngo0805@gmail.com>
2026-03-23 11:55:43 +0700

    feat(ui): update brand__logo at shopping-ads1; shopping-ads2 done;

---
+++ Frontend Intern Training Plan/7_ShoppingAds_remake/js/shopping-ads2.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const upButton = document.getElementById('up-button');
+    const downButton = document.getElementById('down-button');
+    const wrapper = document.querySelector('.shopping-ads__wrapper');
+
+    let autoPlay;
+
+    const getScrollAmount = () =>{
+        const rows = document.querySelectorAll('.items-container');
+        if(rows.length >= 2){
+            return rows[1].offsetTop - rows[0].offsetTop;
+        }
+
+        const row = document.querySelector('.items-container');
+        return row ? row.offsetHeight : 0;
+    };
+
+
+    const slideDown = () =>{
+        const scrollAmount = getScrollAmount();
+
+        if(wrapper.scrollTop + wrapper.clientHeight >= wrapper.scrollHeight - 10){
+            wrapper.scrollTo({top: 0, behavior: 'smooth'});
+        } else{
+            wrapper.scrollBy({top: scrollAmount, behavior: 'smooth'});
+        }
+    };
+
+    const slideUp = () =>{
+        const scrollAmount = getScrollAmount();
+
+        if(wrapper.scrollTop <= 0){
+            wrapper.scrollTo({top: wrapper.scrollHeight, behavior: 'smooth'});
+        } else{
+            wrapper.scrollBy({top: -scrollAmount, behavior: 'smooth'});
+        }
+    };
+
+    const startAutoPlay = () =>{
+        autoPlay = setInterval(slideDown, 3000);
+    };
+
+    const resetAutoPlay = () =>{
+        clearInterval(autoPlay);
+        startAutoPlay();
+    };
+
+    downButton.addEventListener('click', () =>{
+        slideDown();
+        resetAutoPlay();
+    });
+
+    upButton.addEventListener('click', () =>{
+        slideUp();
+        resetAutoPlay();
+    });
+
+    startAutoPlay();
+})
