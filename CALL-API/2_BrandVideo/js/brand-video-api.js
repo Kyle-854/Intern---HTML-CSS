e2b4327bfd08d100646941debebdd3e5309d8f83
Kyle <anhkietngo0805@gmail.com>
2026-03-27 16:59:36 +0700

    cập nhật brand-video-api.js

--- CALL-API/2_BrandVideo/js/brand-video-api.js
+++ CALL-API/2_BrandVideo/js/brand-video-api.js
@@ -1,30 +1,44 @@
 async function loadData() {
     try {
-        const res = await fetch('http://localhost:3000/brand-video');
-        const data = await res.json();
+        const res = await fetch('http://localhost:3000/creatives');
+        const json = await res.json();
 
         //----------------------XỬ LÝ----------------------
-        //url
-            const brandVideoFrame = document.querySelector('.brand-video');
-            brandVideoFrame.addEventListener('click', function(){
-                window.open(data.url, '_blank');
-            });
-        //title
-            document.querySelector('.brand-video__title').innerText = data.title;
-        //poster
-            document.querySelector('.vid').poster = data.poster;
-        //brand__name
-            document.querySelector('.partner').innerText = data.brand__name;
-        //video
-            document.querySelector('.vid').src = data.video;
-            document.querySelector('.vid').load();
+        const data = json.arr.find(item => item.type === 'brand-video');
+        if (data) {
+            //url
+                const brandVideoFrame = document.querySelector('.brand-video');
+                if (brandVideoFrame) {
+                    brandVideoFrame.addEventListener('click', function(){
+                        window.open(data.url, '_blank');
+                    });
+                }
+            //title
+                const bvtitle = document.querySelector('.brand-video__title');
+                if (bvtitle) {
+                    bvtitle.innerText = data.title;
+                }
+            //poster
+                const bvposter = document.querySelector('.vid');
+                if (bvposter) {
+                    bvposter.poster = data.poster;
+                }
+            //brand__name
+                const bvbrandname = document.querySelector('.partner strong');
+                if (bvbrandname) {
+                    bvbrandname.innerText = data.brand__name;
+                }
+            //video
+                const bvvideo = document.querySelector('.vid');
+                if (bvvideo) {
+                    bvvideo.src = data.video;
+                    bvvideo.load();
 
-        // CHẶN CLICK LAN TRUYỀN (Để bấm play/pause video không bị nhảy trang)
-            const videoElement = document.querySelector('.vid');
-            
-            videoElement.addEventListener('click', function(event) {
-                event.stopPropagation(); // Lệnh chặn đứng sự kiện click tại đây
-            });
+                    bvvideo.addEventListener('click', function(event) {
+                        event.stopPropagation(); // Lệnh chặn đứng sự kiện click tại đây
+                    });
+                }
+        }
 
     } catch (error) {
         console.error('Lỗi: ', error);
