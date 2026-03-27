672e67aa2010b04a6cf7880549325be2f6a8ad5d
Kyle <anhkietngo0805@gmail.com>
2026-03-27 16:45:33 +0700

    cập nhật video-ads-api.js

--- CALL-API/6_VideoAds/js/video-ads-api.js
+++ CALL-API/6_VideoAds/js/video-ads-api.js
@@ -1,41 +1,49 @@
 async function loadData() {
-    const res = await fetch('http://localhost:3000/video-ads');
-    const data = await res.json();
+    try {
+        const res = await fetch('http://localhost:3000/creatives');
+        const json = await res.json();
 
-    //---------------------- XỬ LÝ ----------------------
-    //url
-        const videoAdsFrame = document.querySelector('.video-ads');
-        if(videoAdsFrame){
-            videoAdsFrame.addEventListener('click', function(){
-                window.open(data.url, '_blank');
-            });
+        //---------------------- XỬ LÝ ----------------------
+        const data = json.arr.find(item => item.type === 'video-ads');
+        if (data) {
+            //url
+                const videoAdsFrame = document.querySelector('.video-ads');
+                if(videoAdsFrame){
+                    videoAdsFrame.addEventListener('click', function(){
+                        window.open(data.url, '_blank');
+                    });
+                }
+            //title
+                const vid_title = document.querySelector('.video-ads__title');
+                if(vid_title){
+                    vid_title.innerText = data.title;
+                }
+            //img__url
+                const vid_img = document.querySelector('.video-ads__Img img');
+                if(vid_img){
+                    vid_img.src = data.img__url;
+                }
+            //brand__name
+                const vid_brand_name = document.querySelector('.partner strong');
+                if(vid_brand_name){
+                    vid_brand_name.innerText = data.brand__name;
+                }
+            //decription
+                const vid_decription = document.querySelector('.video-ads__decription');
+                if(vid_decription){
+                    vid_decription.innerText = data.decription;
+                }
+            //CHỐNG CLICK NHẦM
+            const closeButton = document.getElementById('video-ads__close-button');
+            if (closeButton) {
+                closeButton.addEventListener('click', function(event){
+                    event.stopPropagation();
+                });
+            }
         }
-    //title
-        const vid_title = document.querySelector('.video-ads__title');
-        if(vid_title){
-            vid_title.innerText = data.title;
-        }
-    //img__url
-        const vid_img = document.querySelector('.video-ads__Img img');
-        if(vid_img){
-            vid_img.src = data.img__url;
-        }
-    //brand__name
-    const vid_brand_name = document.querySelector('.partner strong');
-    if(vid_brand_name){
-        vid_brand_name.innerText = data.brand__name;
+    } catch (error) {
+        console.error('Lỗi: ', error);
     }
-
-    //decription
-    const vid_decription = document.querySelector('.video-ads__decription');
-    if(vid_decription){
-        vid_decription.innerText = data.decription;
-    }
-
-    //CHỐNG CLICK NHẦM
-    const closeButton = document.getElementById('video-ads__close-button');
-    closeButton.addEventListener('click', function(event){
-        event.stopPropagation();
-    });
+    
 }
 loadData();
