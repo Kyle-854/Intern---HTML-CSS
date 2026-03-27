65272eeb63bb096893560938e899757e8975fcfa
Kyle <anhkietngo0805@gmail.com>
2026-03-27 11:57:47 +0700

    thêm file brand-video-api

---
+++ CALL-API/2_BrandVideo/js/brand-video-api.js
@@ -0,0 +1,33 @@
+async function loadData() {
+    try {
+        const res = await fetch('http://localhost:3000/brand-video');
+        const data = await res.json();
+
+        //----------------------XỬ LÝ----------------------
+        //url
+            const brandVideoFrame = document.querySelector('.brand-video');
+            brandVideoFrame.addEventListener('click', function(){
+                window.open(data.url, '_blank');
+            });
+        //title
+            document.querySelector('.brand-video__title').innerText = data.title;
+        //poster
+            document.querySelector('.vid').poster = data.poster;
+        //brand__name
+            document.querySelector('.partner').innerText = data.brand__name;
+        //video
+            document.querySelector('.vid__src').src = data.video; //không có lấy được dữ liệu mới
+
+        // CHẶN CLICK LAN TRUYỀN (Để bấm play/pause video không bị nhảy trang)
+            const videoElement = document.querySelector('.vid');
+            
+            videoElement.addEventListener('click', function(event) {
+                event.stopPropagation(); // Lệnh chặn đứng sự kiện click tại đây
+            });
+
+    } catch (error) {
+        console.error('Lỗi: ', error);
+    }
+}
+
+loadData();
