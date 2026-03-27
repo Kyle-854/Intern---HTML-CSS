f39b566f7a610d957ddff277399bde7b11648ed4
Kyle <anhkietngo0805@gmail.com>
2026-03-27 16:20:02 +0700

    cập nhật native-ads-api.js

--- CALL-API/1_NativeAds/js/native-ads-api.js
+++ CALL-API/1_NativeAds/js/native-ads-api.js
@@ -1,53 +1,56 @@
 async function loadData(){
     try {
-        const res = await fetch('http://localhost:3000/native-ads');
-        const data = await res.json();
+        const res = await fetch('http://localhost:3000/creatives');
+        const json = await res.json();
 
         //XỬ LÝ
-        //url
-        const nativeFrame = document.querySelector('.native');
-        if(nativeFrame){
-            nativeFrame.addEventListener('click', function(){
-                window.open(data.url, '_blank');
-            });
-        }
-        //title
-        const native_title = document.querySelector('.native__title');
-        if (native_title) {
-            native_title.innerText = data.title;
-        }
-        //img
-        const native_img = document.querySelector('.native__img');
-        if (native_img) {
-            native_img.src = data.img__url;
-        }
-        //brand__name
-        const native_brand_name = document.querySelector('.partner');
-        if (native_brand_name) {
-            native_brand_name.innerText = data.brand__name;
-        }
-        //decription
-        const native_decription = document.querySelector('.native__decription');
-        if (native_decription) {
-            native_decription.innerText = data.decription;
-        }
-        //cta
-        const native_cta = document.querySelector('.cta');
-        if (native_cta) {
-            native_cta.innerText = data.cta;
-        }
-        //cta__url
-        const native_cta_url = document.querySelector('.native__register');
-        if (native_cta_url) {
-            native_cta_url.href = data.cta__url;
-            native_cta_url.target = '_blank';
+        const data = json.arr.find(item => item.type === 'native-ads');
+        if (data) {
+            //url
+            const nativeFrame = document.querySelector('.native');
+            if(nativeFrame){
+                nativeFrame.addEventListener('click', function(){
+                    window.open(data.url, '_blank');
+                });
+            }
+            //title
+            const native_title = document.querySelector('.native__title');
+            if (native_title) {
+                native_title.innerText = data.title;
+            }
+            //img
+            const native_img = document.querySelector('.native__img');
+            if (native_img) {
+                native_img.src = data.img__url;
+            }
+            //brand__name
+            const native_brand_name = document.querySelector('.partner');
+            if (native_brand_name) {
+                native_brand_name.innerText = data.brand__name;
+            }
+            //decription
+            const native_decription = document.querySelector('.native__decription');
+            if (native_decription) {
+                native_decription.innerText = data.decription;
+            }
+            //cta
+            const native_cta = document.querySelector('.cta');
+            if (native_cta) {
+                native_cta.innerText = data.cta;
+            }
+            //cta__url
+            const native_cta_url = document.querySelector('.native__register');
+            if (native_cta_url) {
+                native_cta_url.href = data.cta__url;
+                native_cta_url.target = '_blank';
 
-            native_cta_url.addEventListener('click', function(event){
-                event.stopPropagation();
-            });
+                native_cta_url.addEventListener('click', function(event){
+                    event.stopPropagation();
+                });
+            }
+            //brand__img
         }
-
-        //brand__img
+        
 
 
     } catch (error) {
