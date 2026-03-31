bed4c1c1d77ce2f42246197a1e33f1b0230465c6
Kyle <anhkietngo0805@gmail.com>
2026-03-31 11:50:48 +0700

    thêm shopping-ads2-api.js

---
+++ CALL-API/7_ShoppingAds/js/shopping-ads2-api.js
@@ -0,0 +1,99 @@
+async function loadData(limit) {
+    try {
+        const res = await fetch(`http://localhost:3001/creatives?type=shopping-ads&limit=${limit}`);
+        const data = await res.json();
+
+        //--------------XỬ LÝ--------------
+        const wrapper = document.querySelector('.shopping-ads__wrapper');
+        
+        // Lấy 2 cái khuôn ra
+        const tempContainer = document.querySelector('.temp-container');
+        const tempItem = document.querySelector('.temp-item');
+        
+        let currentContainer = null;
+
+        // Vòng lặp đổ dữ liệu
+        data.forEach((itemData, index) => {
+            // Cứ mỗi 2 item, tạo 1 cái Container mới
+            if (index % 2 === 0) {
+                currentContainer = tempContainer.cloneNode(false); 
+                currentContainer.style.display = 'flex';
+                currentContainer.classList.remove('temp-container');
+                wrapper.appendChild(currentContainer);
+            }
+
+            // Nhân bản cái Item
+            const cloneItem = tempItem.cloneNode(true);
+            cloneItem.style.display = 'flex'; 
+            cloneItem.classList.remove('temp-item');
+
+            // --- ĐỔ DỮ LIỆU ---
+            //url
+            if (itemData.url) {
+                cloneItem.addEventListener('click', function(){
+                    window.open(itemData.url, '_blank')
+                });
+            }
+            //img_url
+            const img = cloneItem.querySelector('.item__img img');
+            if (img && itemData.img_url){
+                img.src = itemData.img_url;
+            }
+            //title
+            const name = cloneItem.querySelector('.item__name');
+            if (name && itemData.title){
+                name.innerText = itemData.title;
+            }
+
+            //price2
+                const price2 = cloneItem.querySelector('.price2');
+                if (price2 && itemData.price2) {
+                    price2.innerText = itemData.price2;
+                }
+            //price1
+                const price1 = cloneItem.querySelector('.price1');
+                if (price1 && itemData.price1) {
+                    price1.innerText = itemData.price1;
+                }
+
+                if(!itemData.price1){
+                    const item__price1 = cloneItem.querySelector('.item__price1');
+                    if(item__price1){
+                        item__price1.style.visibility = 'hidden';
+                    }
+                    price2.style.color = '#222222';
+                    const unit2 = cloneItem.querySelector('.unit2');
+                    if(unit2){
+                        unit2.style.color = '#222222';
+                    }
+                }
+            //recommend
+                const recommend = cloneItem.querySelector('.item__recommend');
+                if (recommend && itemData.recommend) {
+                    recommend.innerText = itemData.recommend;
+                }
+
+                if(!itemData.recommend && recommend){
+                    recommend.style.visibility = 'hidden';
+                }
+            //brand_logo
+                const brand_logo = cloneItem.querySelector('.brand__logo');
+                if (brand_logo && itemData.brand_logo) {
+                    brand_logo.src = itemData.brand_logo;
+                }
+            //brand_name
+                const brand_name = cloneItem.querySelector('.brand__name');
+                if (brand_name && itemData.brand_name) {
+                    brand_name.innerText = itemData.brand_name;
+                }
+
+            // Nhét Item vào Container
+            currentContainer.appendChild(cloneItem);
+        });
+        tempContainer.remove();
+        tempItem.remove();
+    } catch (error) {
+        console.error('Lỗi:', error);
+    }
+    
+}
