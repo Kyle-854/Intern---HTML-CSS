7d0eda57d78645b9c8410d1a72e001d8b100b684
Kyle <anhkietngo0805@gmail.com>
2026-04-03 10:50:03 +0700

    xong news5

--- CALL-API/api/news5-jquery-api.js
+++ CALL-API/api/news5-jquery-api.js
@@ -1,24 +1,95 @@
 async function loadData(limit) {
-    try {
-        const data = await $.get(`http://localhost:3001/creatives?type=news&_limit=${limit}`);
-    
-        const contentContainer = $('.news__content-container');
-        contentContainer.empty();
+  try {
+    limit = Math.max(4, Math.min(limit, 12));
+    const data = await $.get(
+      `http://localhost:3001/creatives?type=news&_limit=${limit}`,
+    );
 
-        let container1 = null;
-        let container2 = null;
+    const contentContainer = $(".news__content-container");
+    contentContainer.empty();
 
-        data.forEach((item, index) => {
+    let container1 = null;
+    let container2 = null;
+
+    container1 = $('<div class="news__content1"></div>');
+    contentContainer.append(container1);
+    container2 = $('<div class="news__content2"></div>');
+    contentContainer.append(container2);
+
+    data.forEach((item, index) => {
+      //url
+      const url = item.url ? item.url : "#";
+      //img_url
+      const img_url = item.img_url
+        ? item.img_url
+        : "../assets/images/new_img.png";
+      //title
+      const title = item.title ? item.title : "No Title";
+
+      if (index < 4) {
+        const htmlString1 = `
+            <div class="news__item1">
+                <div class="news__item1__img">
+                    <img src="${img_url}" alt="image">
+                </div>
             
+                <p class="news__title1">
+                    ${title}
+                </p>
+            </div>
+        `;
 
-            container1 = $('<div class="news__content-container"></div>');
-            if (index < 4 ){
-
-            }
-
+        const newsItem1 = $(htmlString1);
+        newsItem1.on("click", function () {
+          window.open(url, "_blank");
         });
-    
-    } catch (error) {
-        console.error('Lỗi: ', error);
+        container1.append(newsItem1);
+      } else {
+        if (index % 2 === 0) {
+          item2_container = $('<div class="news__item2-container"></div>');
+          container2.append(item2_container);
+        }
+
+        const htmlString2 = `
+            <div class="news__item2">
+                <div class="news__item2__img">
+                    <img src="${img_url}" alt="image">
+                </div>
+            
+                <p class="news__title2">
+                    ${title}
+                </p>
+            </div>
+        `;
+
+        const newsItem2 = $(htmlString2);
+        newsItem2.on("click", () => {
+          window.open(url, "_blank");
+        });
+        item2_container.append(newsItem2);
+      }
+    });
+
+    if (limit <= 4) {
+      container2.remove();
+
+      const ctn1 = contentContainer.find(".news__content1");
+      ctn1.css({
+        margin: "0",
+        padding: "0",
+        border: "0",
+      });
     }
+
+    if (limit > 4 && limit % 2 !== 0) {
+      const lastItem = container2.find(".news__item2").last();
+      lastItem.css({
+        margin: "0",
+        padding: "0",
+        border: "0",
+      });
+    }
+  } catch (error) {
+    console.error("Lỗi: ", error);
+  }
 }
