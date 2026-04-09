e962b0df7577c7361dfac2deea13b770ae369967
Kyle <anhkietngo0805@gmail.com>
2026-04-09 15:17:52 +0700

    xong sticky

--- CALL-API/api/sticky-api.js
+++ CALL-API/api/sticky-api.js
@@ -5,26 +5,42 @@ async function loadData() {
 
         //--------------------XỬ LÝ ----------------------
         //url
-            const sticky_url = document.querySelector('.sticky');
-            if (sticky_url && data.url) {
-                sticky_url.addEventListener('click', function() {
-                    window.open(data.url, '_blank');                        
-                });
-            }
+            // const sticky_url = document.querySelector('.sticky');
+            // if (sticky_url && data.url) {
+            //     sticky_url.addEventListener('click', function() {
+            //         window.open(data.url, '_blank');                        
+            //     });
+            // }
+
+            const url = data.url ?? '#';
         //title
             const title = document.querySelector('.sticky__title');
             if (title && data.title) {
                 title.innerText = data.title;
+
+                title.addEventListener('click', () => {
+                    window.open(url, '_blank');
+                });
             }
         //img__url
             const sticky_img  = document.querySelector('.sticky__img img');
             if (sticky_img && data.img_url) {
                 sticky_img.src = data.img_url;
+
+                sticky_img.addEventListener('click', () => {
+                    window.open(url, '_blank');
+                });
             }
         //brand__name
             const brand_name = document.querySelector('.partner strong');
             if(brand_name && data.brand_name) {
                 brand_name.innerText = data.brand_name;
+            }
+        //brand_url
+            const brand_url = data.brand_url ?? '#';
+            const partner = document.querySelector('.partner');
+            if (partner) {
+                partner.href = brand_url;
             }
         //cta, cta__url
             const cta = document.querySelector('.sticky__button-cta');
