1450856e4de13874e9c65106ec3f2111b62ed39b
Kyle <anhkietngo0805@gmail.com>
2026-04-10 17:36:09 +0700

    gần xong cái dropdown-menu  trong sign-in-mobile

--- Authentication-Forms/Sign-in-form/js/sign-in-mobile.js
+++ Authentication-Forms/Sign-in-form/js/sign-in-mobile.js
@@ -2,5 +2,11 @@ const menu = document.querySelector('.menu');
 const dropdown = document.querySelector('.dropdown');
 
 menu.addEventListener('click', () => {
-  dropdown.classList.toggle('show');
+    const temp = window.getComputedStyle(dropdown).visibility;
+    
+    if (temp === 'hidden'){
+        dropdown.style.visibility = 'visible';
+    } else {
+        dropdown.style.visibility = 'hidden';
+    }
 });
