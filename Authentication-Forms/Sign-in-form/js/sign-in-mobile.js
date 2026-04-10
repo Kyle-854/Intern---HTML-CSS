1ccd338b028baa60b7efcd4c9b8555045411c88d
Kyle <anhkietngo0805@gmail.com>
2026-04-10 17:43:47 +0700

    gần xong cái dropdown-menu

--- Authentication-Forms/Sign-in-form/js/sign-in-mobile.js
+++ Authentication-Forms/Sign-in-form/js/sign-in-mobile.js
@@ -1,12 +1,15 @@
-const menu = document.querySelector('.menu');
-const dropdown = document.querySelector('.dropdown');
+function dropdownMenu(){
+    const menu = document.querySelector('.menu');
+    const dropdown = document.querySelector('.dropdown');
 
-menu.addEventListener('click', () => {
-    const temp = window.getComputedStyle(dropdown).visibility;
-    
-    if (temp === 'hidden'){
-        dropdown.style.visibility = 'visible';
-    } else {
-        dropdown.style.visibility = 'hidden';
-    }
-});
+    menu.addEventListener('click', () => {
+        const temp = window.getComputedStyle(dropdown).visibility;
+
+        if (temp === 'hidden'){
+            dropdown.style.visibility = 'visible';
+        } else {
+            dropdown.style.visibility = 'hidden';
+        }
+    });
+}
+dropdownMenu();
